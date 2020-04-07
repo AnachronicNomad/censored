6edbb870b120d9ee28ce2c53849030da6bdf59db
anachronicnomad <anachronicnomad@gmail.com>
2020-04-07 16:41:21 -0600

    have it read timestamps from file

--- censor.py
+++ censor.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
 import subprocess
 
-times = [(7,8), (13,14)]
+times = []
 
 full_time = 18
+
+with open("censor_times") as f:
+	for line in f.readlines():
+		start = 0
+		end = 0
+		x = line.split(",")
+		y = x[0].split(":")
+		z = x[1].split(":")
+		start = (3600 * int(y[0])) + (60 * int(y[1])) + int(y[2])
+		print(start)
+		end = (3600 * int(z[0])) + (60 * int(z[1])) + int(z[2])
+		print(end)
+		times.append((start, end))
+
 
 for time in times:
 	start = time[0]
@@ -16,6 +30,10 @@ for time in times:
 						   "-vcodec",
 						   "copy", 
 						   "out1.mkv"])
+	proc.wait()
+	proc = subprocess.Popen(["mv",
+		 					 "out1.mkv",
+		 					 "whatever.mkv"])
 	proc.wait()
 
 
